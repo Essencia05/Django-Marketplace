4d3239a4780c82eb6ada4d2f2906afe8871fc1f4
Essencia05 <re.letlape@gmail.com>
2024-06-01 03:53:40 +0200

    login and add

--- puddle/puddle/settings.py
+++ puddle/puddle/settings.py
@@ -27,6 +27,9 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_URL = '/login/'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
 
 # Application definition
 
@@ -118,8 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
 
-# Default primary key field type
+# Default primary ;p0key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
--- puddle/puddle/urls.py
+++ puddle/puddle/urls.py
@@ -1,11 +1,13 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from core.views import index, contact
+#from core.views import index, contact// dont need to import anymore
 
 urlpatterns = [
    
-    path('', index, name = 'index'),
-    path('contact/', contact, name = 'contact'),
+    path('', include('core.urls')),   
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
